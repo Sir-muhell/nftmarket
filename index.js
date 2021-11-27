d0d650b3f8d9f667ea056614567345271d862c62
inspi-writer001 <conservedinnovation@gmail.com>
2021-11-27 13:37:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+console.log('Server-side code running');
 const mongoose = require('mongoose');
+const nameduser = require('../nftmarket/models/newUser')
 
-mongoose.connect("mongodb://localhost:27017/User1", (error) =>{
-    if(!error){
-        console.log("Success");
-    }
-    else{
-        console.log("Error connecting to database");
-    }
+app.use('')
+
+const express = require('express');
+
+
+const app = express();
+
+const DB_URI = 'mongodb+srv://rootuser:rootpass@cluster0.0fksq.mongodb.net/AllUsers?retryWrites=true&w=majority';
+mongoose.connect(DB_URI)
+    .then((result) => app.listen(8080, () => {
+        console.log('listening on 8080');
+      }))
+      .then((rese) => console.log('connected to db'))
+    .catch((err) => console.log(err));
+
+    //serve files from the public directory
+
+app.use(express.static('public'));
+
+
+
+//serve the homepage
+app.get('/link-wallet', (req, res) => {
+  const uuser = new Newuser{
+      username: 'james'
+
+  }
 });
